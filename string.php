4db1c99895c9d7da39d64b6c82a2d0ea9ab85ba9
Ruma303 <matteoh8@gmail.com>
2023-07-17 12:45:33 +0200

    metodi stringhe

--- string.php
+++ string.php
@@ -65,7 +65,6 @@ echo $nowdoc; */
 // Ciao, il mio nome è Matteo " Piacere di conoscerti! '
 
 
-
 //$ Nowdoc
 /* $nome = "Matteo";
 $nowdoc = <<<'Text'
@@ -76,11 +75,74 @@ echo $nowdoc; */
 // Ciao, il mio nome è $nome.
 
 
+//% Costrutti delle stringhe
+?> <!-- Occorre uscire da PHP -->
+<html>
+
+    <!-- echo -->
+  <!--   <?php
+        $name = "Matteo";
+        $age = 28;
+        echo $name, $age;
+    ?>
+    <h2><?= 'Hello World'; ?><h2>
+    <?= '<h2>Hello World</h2>'; ?>
+ -->
+
+    <!-- print -->
+    <?php //print $name; ?>
+</html>
+
+<?php
+
+
 
 //% Metodi delle stringhe
 
-//$ str_replace()
 /* $originale = "Ciao, mondo!";
 $sostituita = str_replace("mondo", "PHP", $originale);
 echo $originale; // "Ciao, mondo!"
 echo $sostituita; // "Ciao, PHP!" */
+
+$str = "Stringa di prova";
+
+//echo strlen($str); // 16
+
+//echo substr($str, 5); // ga di prova
+//echo substr($str, 5, 4); // ga ds
+
+//echo str_replace("Stringa", "Testo", $str);
+// Testo di prova
+
+//echo strpos($str, "di"); // 8
+
+/* $str2 = "Ciao";
+echo strcmp($str, $str2); // 1 */
+
+/* $array = explode(" ", $str);
+var_dump($array); */
+
+/* $text = "L'apprendimento di 'PHP' è divertente!";
+echo addslashes($text);
+// L\'apprendimento di \'PHP\' è divertente! */
+
+/* echo trim("  Ciao, mondo!  ");   // "Ciao, mondo!"
+echo ltrim("  Ciao, mondo!  ");  // "Ciao, mondo!  "
+echo rtrim("  Ciao, mondo!  ");  // "  Ciao, mondo!" */
+
+/* echo lcfirst("Ciao, mondo!"); // "ciao, mondo!"
+echo ucfirst("ciao, mondo!"); // "Ciao, mondo!"
+echo ucwords("ciao, mondo!"); // "Ciao, Mondo!"
+echo ucwords("ciao-mondo!", "-"); // "Ciao-Mondo!" */
+
+/* echo strtolower("Città");   // "città"
+echo strtoupper("città");   // "CITTà"
+echo mb_strtolower("CITTÀ"); // "città"
+echo mb_strtoupper("città"); // "CITTÀ" */
+
+
+//$ Metodi disponibili dopo PHP 8
+//echo str_contains("Ciao, mondo!", "mondo"); // true
+//echo str_starts_with("Ciao, mondo!", "Ciao"); // true
+//echo str_ends_with("Ciao, mondo!", "mondo!"); // true
+//echo get_debug_type("Ciao, mondo!"); // "string"
